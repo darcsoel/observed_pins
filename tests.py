96c2a79880d39401ed9490ea82b9651ee95ca45f
Ross <rostikvunarchuk@gmail.com>
2022-03-27 23:29:14 +0300

    init

--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
 # import pytest
+from main import get_pins
+
+
+def test1():
+    input_data = "8"
+    expected = ["5", "7", "8", "9", "0"]
+    result = get_pins(input_data)
+    assert result == expected
+
+
+def test2():
+    input_data = "11"
+    expected = ["11", "22", "44", "12", "21", "14", "41", "24", "42"]
+    result = get_pins(input_data)
+    assert result == expected
